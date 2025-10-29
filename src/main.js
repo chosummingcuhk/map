404a3bc6847ef70472a5ca2ce249bd8268a392f6
Ron <1155176719@link.cuhk.edu.hk>
2025-10-29 16:52:08 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// var map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([22.28552, 114.15769], 13);
+
+// Add Hong Kong basemap with traditional Chinese labels
+const hkLayer = L.layerGroup.hongKong("topography.tc");
+hkLayer.addTo(map);
+// const map = L.map('map').setView([22.28552, 114.15769], 13);
+//
+// // Add OpenStreetMap tile layer
+// const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="httpsenstreetmap.org/copyright contributors'
+// });
+//
+// osmLayer.addTo(map);
